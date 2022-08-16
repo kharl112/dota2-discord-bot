c576b03f7c468a5c46add720da5061738bf7124c
kharl112 <kharl.dms112@gmail.com>
2022-08-15 21:45:57 -0500

    User model initialized in ready.js

--- event-handlers/events/ready.js
+++ event-handlers/events/ready.js
@@ -1,8 +1,12 @@
+const User = require("../../database/models/user");
+
 module.exports = {
   name: "ready",
   //identifies if execute function will execute once
   once: true,
   execute(client) {
+    //define table users
+    User.sync();
     console.log(`${client.user.tag} has arise from death`);
   },
 };
