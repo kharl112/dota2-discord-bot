484d77758e88a7181a87ac856fb951e6467f74bb
kharl112 <kharl112>
2024-06-01 16:02:35 +0000

    search player completed

--- command-builders/controllers/players.js
+++ command-builders/controllers/players.js
@@ -13,13 +13,39 @@ module.exports = (() => {
           ({ name }) => name === SEARCH_PLAYER
       );
 
-      if(!search_input){
-        await interaction.reply({ content: 'No results found.', ephemeral: true });
-        return false;
-      }  
+      try {
 
-      await interaction.reply(search_input.value);
-      return true;
+        //no search inputs
+        if(!search_input) throw 'No results found';
+        if(!search_input.value) throw 'No results found';
+      
+        //prevent getting unknown interaction error
+        await interaction.deferReply();
+
+        //get player basic info 
+        const request = await axios.get(`${process.env.OPEN_DOTA_URL}/search?q=${search_input.value}`);
+        if (!request.data[0]) throw 'No results found';
+
+        //get player full info
+        const player = request.data[0];
+
+        const player_embed = new EmbedBuilder()
+          .setColor(0x1ccc17)
+          .setTitle(player.personaname)
+          .setThumbnail(player.avatarfull)
+          .addFields(
+            {
+              name: "Last Match",
+              value:  new Date(player.last_match_time).toLocaleString("en-us", { dateStyle: "medium" }),
+              inline: true,
+            }
+          );
+
+        await interaction.editReply({ embeds: [player_embed] });
+      }catch(error){
+        console.log(error)
+        await interaction.editReply({ content: error.toString(), ephemeral: true });
+      }
   };
 
 
